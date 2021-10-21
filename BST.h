02702706277df718b1f23dc1b4421b2b9d53de2d
ricardordz13 <70780230+ricardordz13@users.noreply.github.com>
2021-10-21 12:00:21 -0500

    Create BST.h
    BST file

---
+++ BST.h
@@ -0,0 +1,61 @@
+#include "NodeT.h"
+
+class BST{
+	public:
+		BST();
+		~BST();
+		void add(int data);
+		void print();
+	private:
+		NodeT *root;
+		void inOrdenC(NodeT *r);
+		void destuye_Helper(NodeT *r);
+};
+
+BST::BST(){
+	root = nullptr;
+}
+
+BST::~BST(){
+	destuye_Helper(root);
+}
+
+void BST::destuye_Helper(NodeT *r){
+	if (r != nullptr){
+		destuye_Helper(r->getLeft());
+		destuye_Helper(r->getRight());
+		delete r;
+	}
+}
+
+void BST::add(int data){
+	if (root == nullptr){
+		root = new NodeT(data);
+	}
+	else{
+		NodeT *curr = root;
+		NodeT *parent = nullptr;
+		while (curr != nullptr){
+			if (curr->getData() == data){
+				return;
+			}
+			parent = curr;
+			curr = (curr->getData() > data) ? curr->getLeft() : curr->getRight();
+		}
+		parent->getData() > data ? parent->setLeft(new NodeT(data)) : parent->setRight(new NodeT(data));
+	}
+}
+
+void BST::print(){
+	inOrdenC(root);
+	cout << endl;
+}
+
+void BST::inOrdenC(NodeT *r){
+	if (r != nullptr){
+		inOrdenC(r->getRight());
+		cout << r->getData() << " ";
+		inOrdenC(r->getLeft());
+	}
+}
+
