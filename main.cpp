97c841de8c9c89afdef502fc9d099fac9c800c66
ricardordz13 <70780230+ricardordz13@users.noreply.github.com>
2021-10-21 23:32:40 -0500

    Add the loadData

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 
 using namespace std;
 
 #include "BST_3_4.h"
 
-void loadData(dataCS &dataLoaded, BST &arbol, string txtFile)
+void loadData(dataCS &dataLoaded, BST &arbol, string txtFile, queue<string> &qu)
 {
 	ifstream dataSuez(txtFile);
-	while(dataSuez >> dataLoaded.nombre >> dataLoaded.fecha >> dataLoaded.hora >> dataLoaded.tipo)
+	while(dataSuez >> dataLoaded.ubi >> dataLoaded.fecha >> dataLoaded.hora >> dataLoaded.tipo)
 	{
-		arbol.add(dataLoaded);
-	}
-	dataSuez.close();
+        dataLoaded.nombre = dataLoaded.ubi.substr(0,3);
+        dataLoaded.cant = arbol.repetidos(dataLoaded.nombre) + 1;
+        arbol.add(dataLoaded);
+    }
+    dataSuez.close();
 }
 
 int main()
@@ -21,7 +24,10 @@ int main()
     BST arbolR;
 	struct dataCS dataSuez;
 
-	// preguntamos por el nombre del archivo de texto
+    queue<string> quM;
+    queue<string> quR;
+
+	// preguntamos por el nombre de los archivos de texto
 	string txtFileM, txtFileR;
 	cout << "¿Cuál es el nombre del archivo de texto de donde se sacarán los datos del Mar Mediterráneo? (ej. canal.txt): ";
 	cin >> txtFileM;
@@ -29,15 +35,13 @@ int main()
 	cin >> txtFileR;
 
 	// cargamos los datos 
-	loadData(dataSuez, arbolM, txtFileM);
+	loadData(dataSuez, arbolM, txtFileM, quM);
 
     cout << "Entradas Mar ROJO" << endl;
     arbolM.print();
 
-    loadData(dataSuez, arbolR, txtFileR);
-    
+    loadData(dataSuez, arbolR, txtFileR, quR);
+
     cout << "Entradas Mar MEDITERRANEO" << endl;
     arbolR.print();
-	
 }
-
